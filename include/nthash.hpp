51cc05066be4698b9d417b093603f5e6b8f5ff5d
mxwang66 <54554363+mxwang66@users.noreply.github.com>
2026-03-27 11:09:05 -0500

    Initial commit

---
+++ include/nthash.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <btllib/hashing_internals.hpp>
+#include <btllib/nthash_kmer.hpp>
+#include <btllib/nthash_seed.hpp>
+#include <btllib/status.hpp>
+
+namespace btllib {
+
+/**
+ * String representing the hash function's name. Only change if hash outputs
+ * are different from the previous version. Useful for tracking differences in
+ * saved hashes, e.g., in Bloom filters.
+ */
+static const char* const NTHASH_FN_NAME = "ntHash_v2";
+
+} // namespace btllib
